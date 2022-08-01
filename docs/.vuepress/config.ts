4c1f9768760816c9ada3397634750e5f5b9a4232
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2022-08-02 00:49:36 +0800

    import files

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,57 @@
+import { defineUserConfig } from "vuepress";
+import { defaultTheme } from "@vuepress/theme-default";
+import { backToTopPlugin } from "@vuepress/plugin-back-to-top";
+
+export default defineUserConfig({
+  lang: "zh-CN",
+  title: "伽马影迷插件",
+  description: "影迷、影视自媒体必备神器",
+  theme: defaultTheme({
+    logo: "/assets/logo.png",
+    navbar: [
+      {
+        text: "安装指南",
+        link: "/install/",
+      },
+
+      {
+        text: "使用指南",
+        link: "/usage/",
+      },
+      {
+        text: "常见问题",
+        link: "/faqs/",
+      },
+      {
+        text: "反馈建议",
+        link: "https://support.qq.com/products/316535",
+      },
+
+      {
+        text: "应用商店",
+        children: [
+          {
+            text: "edge",
+            link: "https://microsoftedge.microsoft.com/addons/detail/cmnchflmcededjokdbjafjcpbhpodmmo",
+          },
+          {
+            text: "chrome",
+            link: "https://chrome.google.com/webstore/detail/jwksext-the-missing-tool/ehmoihnjgkdimihkhokkmfjdgomohjgm",
+          },
+          {
+            text: "safari",
+            link: "https://apps.apple.com/cn/app/apple-store/id1611761892",
+          },
+          {
+            text: "firefox",
+            link: "https://addons.mozilla.org/zh-CN/firefox/addon/jwksext-best-for-movie-fans/",
+          },
+        ],
+      },
+    ],
+    docsBranch: "master",
+    editLinkText: "编辑此页面",
+    repo: "https://github.com/ggt1024/extdocs",
+  }),
+  plugins: [backToTopPlugin()],
+});
---
+++ docs/README.md
@@ -0,0 +1,27 @@
+---
+home: true
+# heroImage: ./assets/overview.v22.6.1.png
+heroText: 伽马影迷插件
+tagline: 是一个在电脑浏览器中使用的小程序，帮助电影迷快速了解一部电影。
+actionText: 使用指南 →
+actionLink: ./usage/
+features:
+  - title: 查评分
+    details: 一键查大众评分和专业评分数据
+  - title: 看解说
+    details: 三分钟了解一部电影剧情、比2倍数更快刷剧
+  - title: 找片源
+    details: 既可在线看新片正版，也可下载高清无码无删减资源
+actions:
+  - text: 安装指南
+    link: /install/
+    type: primary
+  - text: 使用指南
+    link: /usage/
+    type: secondary
+  - text: 常见问题解决方法
+    link: /faqs/
+    type: secondary
+footer: Copyright &copy; 2021~ Guangzhou GamMaan Tech LLC
+footerHtml: true
+---
