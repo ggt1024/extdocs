4ce0d5289de66c7ce8fe6ef0890ce25efc4a0fb3
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2022-08-02 01:15:50 +0800

    add pack script

---
+++ pack.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+const zip = require("zip-a-folder").zip;
+
+const fnManifest = "package.json";
+const pathDist = path.join("docs", ".vuepress", "dist");
+
+const manifest = JSON.parse(fs.readFileSync(fnManifest, "utf8"));
+
+const folderName = manifest.name;
+const filePathSaveTo = path.join(folderName + ".zip");
+
+zip(pathDist, filePathSaveTo)
+  .then(() => {
+    console.log(`save to ${filePathSaveTo}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
